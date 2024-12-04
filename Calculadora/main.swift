4454b34e2746e432a40816f4e0b2a79b2e2a06b0
Merianni Nunez Tejeda <merianni09@gmail.com>
2024-12-04 09:28:13 -0400

    MySwiftCalculator initial commit

---
+++ Calculadora/main.swift
@@ -0,0 +1,68 @@
+//
+//  main.swift
+//  Calculadora
+//
+//  Created by Merianni Nunez Tejeda on 12/3/24.
+//
+
+import Foundation
+
+// Calculator menu
+func menu() {
+    let title = "Calculator"
+    print("___________")
+    print("\(title)")
+    print("___________")
+    print("Enter a symbol to perform an operation: (+, -, *, /)")
+    print("""
+      + | Add
+      - | Substract
+      * | Multiply
+      / | Divide
+    """)
+}
+
+// user number input
+func getInput() -> Double {
+    print("Enter a number:")
+    return Double(readLine()!)!
+}
+
+func calculate(_ operation: String, number1: Double, number2: Double) -> String {
+    var result: Double
+    switch operation {
+    case "+":
+        result = number1 + number2
+    case "-":
+        result = number1 - number2
+    case "*":
+        result = number1 * number2
+    case "/":
+        //
+        if number2 == 0 {
+            return "ERROR: Cannot divide by zero"
+        }
+        result = number1 / number2
+    default:
+        return "Invalid operation"
+    }
+    return "\(number1) \(operation) \(number2) = \(result)"
+}
+
+// loop for menu / performing more operations
+while true {
+    menu()
+    let operation = readLine()!
+    let number1 = getInput()
+    let number2 = getInput()
+    let result = calculate(operation, number1: number1, number2: number2)
+    print(result)
+    
+    print("""
+        
+    Do you want to perform another operation? (y/n)
+    """)
+    if readLine()! != "y" {
+        break
+    }
+}
